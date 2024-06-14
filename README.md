020c2cc8cbb5abbcbd6183020caadb5b73fadab3
sinhp <sinahazratpour@gmail.com>
2024-06-14 09:38:48 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# The Groupoid Model of HoTT in Lean 4
+
+Using the ideas from Awodey's Natual Model of Dependent this repository formalizes in Lean4 the groupoid model in HoTT. 
+
+This repository relies on the [formalization of polynomial functors](https://github.com/sinhp/Poly/tree/master). 
---
+++ lakefile.lean
@@ -0,0 +1,16 @@
+import Lake
+open Lake DSL
+
+package «groupoid_model_in_lean4» where
+  -- Settings applied to both builds and interactive editing
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
+  ]
+  -- add any additional package configuration options here
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
+@[default_target]
+lean_lib «GroupoidModelInLean4» where
+  -- add any library configuration options here
