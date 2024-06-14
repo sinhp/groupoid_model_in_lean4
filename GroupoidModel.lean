eb2ff68c3e8a5d6b92f1fb40f16a6c46365d0914
Steve Awodey <sawodey@gmail.com>
2024-06-14 14:02:37 +0200

    organization

--- GroupoidModel.lean
+++ GroupoidModel.lean
@@ -1,3 +1,9 @@
 -- This module serves as the root of the `GroupoidModelInLean4` library.
 -- Import modules here that should be built as part of the library.
 import «GroupoidModelInLean4».Basic
+
+/- There should be at least three separate files here for three separate developments:
+  - the basic development of the category Grpd of groupoids
+  - the intermediate natural model of HoTT which is constructed out of Gpds
+  - the metaprogram level HoTT0 for interacting with the model
+  -/
