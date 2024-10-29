93770fd6e0a7d1df034be9e715238afe3c9aa397
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2024-10-28 23:18:44 -0400

    chore: more cleanup

--- GroupoidModel.lean
+++ GroupoidModel.lean
@@ -1,7 +1,6 @@
 -- This module serves as the root of the `GroupoidModelInLean4` library.
 -- Import modules here that should be built as part of the library.
-import GroupoidModel.Basic
---import GroupoidModel.NaturalModel
+import GroupoidModel.NaturalModel
 
 /- There should be at least three separate files here for three separate developments:
   1. the basic development of the category Grpd of groupoids
