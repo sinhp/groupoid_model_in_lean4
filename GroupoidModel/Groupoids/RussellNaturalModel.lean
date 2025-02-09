2e56b7eea8d30189ac39684f765dc46c0405ab9e
Joseph Hua <josephhua73@yahoo.com>
2025-02-08 19:28:58 -0500

    feat: GroupoidNM uSeqHom

--- GroupoidModel/Groupoids/RussellNaturalModel.lean
+++ GroupoidModel/Groupoids/RussellNaturalModel.lean
@@ -1,4 +1,5 @@
 import Mathlib.CategoryTheory.Limits.Preserves.FunctorCategory
+import Mathlib.CategoryTheory.Core
 
 import GroupoidModel.Russell_PER_MS.UHom
 import GroupoidModel.Grothendieck.IsPullback
@@ -9,8 +10,9 @@ import GroupoidModel.Grothendieck.Groupoidal
 Here we construct the natural model for groupoids.
 -/
 
-universe u v u₁ v₁ u₂ v₂
+universe w v v₁ v₂ u u₁ u₂
 
+noncomputable section
 open CategoryTheory ULift Grothendieck
 
 /-
@@ -19,197 +21,68 @@ the category of
 {small groupoids - size u objects and size u hom sets}
 which itself has size u+1 objects (small groupoids)
 and size u hom sets (functors).
-We want this to be a small category so we lift the homs.
+
+We want our context category to be a small category so we will use
+`AsSmall.{u}` for some large enough `u`
 -/
 
 namespace CategoryTheory
-namespace GroupoidNM
-abbrev Ctx := AsSmall.{u} Grpd.{u,u}
 
-def Ctx.ofGroupoid (C : Type u) [Groupoid.{u} C] : Ctx.{u} :=
-  AsSmall.up.obj (Grpd.of C)
+instance (Γ : Grpd.{v, w}) : Groupoid (AsSmall.{max w u v} Γ) where
+  inv f := AsSmall.up.map (inv (AsSmall.down.map f))
 
-def Ctx.toGrpd (Γ : Ctx.{u}) : Grpd.{u,u} := AsSmall.down.obj Γ
+def Grpd.asSmallFunctor : Grpd.{v, u} ⥤ Grpd.{max w v u, max w v u} where
+  obj Γ := Grpd.of $ AsSmall.{max w v u} Γ
+  map F := AsSmall.down ⋙ F ⋙ AsSmall.up
 
-@[simps] def catLift : Cat.{u,u} ⥤ Cat.{max v u,(max v u) + 1} where
-  obj x := Cat.of (ULiftHom.{v} (ULift.{max v u + 1, u} x))
-  map {x y} f := downFunctor ⋙ f ⋙ upFunctor
+namespace Core
 
-variable (C D) [Category.{u} C] [Category.{u} D]
+variable {C : Type u} [Category.{v} C] 
 
-abbrev yonedaCat : Cat.{u,u+1} ⥤ Ctx.{u}ᵒᵖ ⥤ Type (u + 1) :=
-  yoneda ⋙ (whiskeringLeft _ _ _).obj
-    (AsSmall.down ⋙ Grpd.forgetToCat ⋙ catLift).op
-
-instance yonedaCatPreservesLim : Limits.PreservesLimits yonedaCat :=
-  Limits.comp_preservesLimits _ _
-
-variable {Γ Δ : Ctx.{u}} {C D : Type (u+1)}
-  [Category.{u,u+1} C] [Category.{u,u+1} D]
-
-/- The bijection y(Γ) → [-,C]   ≃   Γ ⥤ C -/
-def yonedaCatEquiv :
-    (yoneda.obj Γ ⟶ yonedaCat.obj (Cat.of C))
-      ≃ Γ.toGrpd ⥤ C :=
-  Equiv.trans yonedaEquiv
-    {toFun     := λ A ↦ ULift.upFunctor ⋙ A
-     invFun    := λ A ↦ ULift.downFunctor ⋙ A
-     left_inv  := λ _ ↦ rfl
-     right_inv := λ _ ↦ rfl}
-
-theorem yonedaCatEquiv_naturality
-    (A : yoneda.obj Γ ⟶ yonedaCat.obj (Cat.of C)) (σ : Δ ⟶ Γ) :
-    (AsSmall.down.map σ) ⋙ yonedaCatEquiv A
-      = yonedaCatEquiv (yoneda.map σ ≫ A) := by
-  simp only [AsSmall.down_obj, AsSmall.down_map, yonedaCatEquiv,
-    Functor.op_obj, Functor.comp_obj, Cat.of_α,
-    Equiv.trans_apply, Equiv.coe_fn_mk, ← yonedaEquiv_naturality]
+@[simp]
+theorem id_inv (X : C) :
+    Iso.inv (coreCategory.id X) = @CategoryStruct.id C _ X := by
   rfl
 
-theorem yonedaCatEquiv_comp
-    (A : yoneda.obj Γ ⟶ yonedaCat.obj (Cat.of D)) (U : D ⥤ C) :
-    yonedaCatEquiv (A ≫ yonedaCat.map U) = yonedaCatEquiv A ⋙ U := by
-  aesop_cat
+@[simp]
+theorem comp_inv {X Y Z : Core C} (f : X ⟶ Y) (g : Y ⟶ Z) :
+    (f ≫ g).inv = g.inv ≫ f.inv :=
+  rfl
 
-abbrev Ty : Psh Ctx.{u} := yonedaCat.obj (Cat.of Grpd.{u,u})
+def functor : Cat.{v,u} ⥤ Grpd.{v,u} where
+  obj C := Grpd.of (Core C)
+  map F := {
+    obj := F.obj
+    map f := {
+      hom := F.map f.hom
+      inv := F.map f.inv}
+    map_id x := by
+      simp only [Grpd.coe_of, id_hom, Functor.map_id, id_inv]
+      congr 1
+    map_comp f g := by
+      simp only [Grpd.coe_of, comp_hom, Functor.map_comp, comp_inv]
+      congr 1}
 
-abbrev Tm : Psh Ctx.{u} := yonedaCat.obj (Cat.of PGrpd.{u,u})
+end Core
 
-variable {Γ : Ctx.{u}} (A : yoneda.obj Γ ⟶ Ty)
-
-abbrev tp : Tm ⟶ Ty := yonedaCat.map (PGrpd.forgetToGrpd)
-
-abbrev ext : Ctx := Ctx.ofGroupoid (Groupoidal (yonedaCatEquiv A))
-
-abbrev disp : ext A ⟶ Γ :=
-  AsSmall.up.map (Grothendieck.forget (yonedaCatEquiv A ⋙ Grpd.forgetToCat))
-
-abbrev var : (yoneda.obj (ext A) : Psh Ctx) ⟶ Tm :=
-  yonedaCatEquiv.invFun (Groupoidal.toPGrpd (yonedaCatEquiv A))
-
-/-- The image of (roughly) `Groupoidal.toPGrpd : Grothendieck A ⥤ PGrpd`
-  under `yonedaCat`.
-  Used in the pullback diagram `isPullback_yonedaCatULiftGrothendieckForget_tp`
--/
-abbrev yonedaCatMapToPGrpd :
-    yonedaCat.obj (IsPullback.uLiftGrothendieck $ Groupoidal.toCat (yonedaCatEquiv A)) ⟶ Tm :=
-  yonedaCat.map
-      (Cat.homOf (ULift.downFunctor ⋙ Groupoidal.toPGrpd (yonedaCatEquiv A)))
-
-/-- The image of (roughly) `Grothendieck.forget : Grothendieck A ⥤ Γ` under
-  `yonedaCat`.
-  Used in the pullback diagram `isPullback_yonedaCatULiftGrothendieckForget_tp`
--/
-abbrev yonedaCatMapGrothendieckForget := 
-      (yonedaCat.map $ IsPullback.uLiftGrothendieckForget
-        (Groupoidal.toCat.{u} $ yonedaCatEquiv A))
-
-/-- The image of `yonedaCatEquiv A` under `yonedaCat`.
-  Used in the pullback diagram `isPullback_yonedaCatULiftGrothendieckForget_tp`
--/
-abbrev yonedaCatMapYonedaCatEquiv :
-    yonedaCat.obj (IsPullback.uLiftΓ.{u,u} Γ.toGrpd) ⟶ Ty :=
-  yonedaCat.map (Cat.homOf (ULift.downFunctor.{u,u} ⋙ (yonedaCatEquiv A)))
-
-/-- The image of the pullback `Grothendieck.Groupoidal.isPullback` under `yonedaCat` -/
-theorem isPullback_yonedaCatGrothendieckForget_tp :
-    IsPullback
-      (yonedaCatMapToPGrpd A)
-      (yonedaCatMapGrothendieckForget A)
-      tp
-      (yonedaCatMapYonedaCatEquiv A) :=
-    Functor.map_isPullback _ (Groupoidal.isPullback _)
-
-def asSmallUp_comp_yoneda_iso_forgetToCat_comp_catLift_comp_yonedaCat :
-    (AsSmall.up) ⋙ (yoneda : Ctx.{u} ⥤ Ctx.{u}ᵒᵖ ⥤ Type (u + 1))
-    ≅ Grpd.forgetToCat ⋙ catLift ⋙ yonedaCat where
-  hom := {app Γ := yonedaEquiv.symm (CategoryStruct.id _)}
-  inv := {
-    app Γ := {
-      app Δ := λ F ↦ AsSmall.up.map $ ULift.upFunctor ⋙ F ⋙ ULift.downFunctor}}
-
-/-- `yoneda.map (disp A)` is isomorphic to `yonedaCat(uLiftGrothendieckForget _)` in
-  the arrow category, hence forming a pullback square
-
-  yoneda (ext A) ------≅----> yonedaCat(uLift (ext A))
-         |                                |
-         |                                |
-         |                                |
-         |                                |
-         |                                |
-         v                                v
-      yoneda Γ --------≅----> yonedaCat(uLift Γ)
--/
-theorem isPullback_yonedaDisp_yonedaCatULiftGrothendieckForget :
-    IsPullback
-      (asSmallUp_comp_yoneda_iso_forgetToCat_comp_catLift_comp_yonedaCat.hom.app _)
-      (yoneda.map (disp A))
-      (yonedaCatMapGrothendieckForget A)
-      (asSmallUp_comp_yoneda_iso_forgetToCat_comp_catLift_comp_yonedaCat.hom.app Γ.toGrpd)
-      :=
-    IsPullback.of_horiz_isIso ⟨
-      asSmallUp_comp_yoneda_iso_forgetToCat_comp_catLift_comp_yonedaCat.hom.naturality
-      (AsSmall.down.map (disp A))⟩
-
-theorem isPullback_yonedaDisp_tp :
-    IsPullback (var A) (yoneda.map (disp A)) tp A := by
-  convert IsPullback.paste_horiz
-    (isPullback_yonedaDisp_yonedaCatULiftGrothendieckForget A)
-    (isPullback_yonedaCatGrothendieckForget_tp _)
-  ext Δ F
-  exact congr_fun (@A.naturality (Opposite.op Γ) Δ F.op) (CategoryStruct.id Γ)
-
-end GroupoidNM
-
-open GroupoidNM
-
--- TODO link to `CategoryTheory.GroupidNMBase` in blueprint
-def groupoidNMBase0 : NaturalModelBase Ctx.{u} where
-  Ty := Ty
-  Tm := Tm
-  tp := tp
-  ext := ext
-  disp := disp
-  var := var
-  disp_pullback := isPullback_yonedaDisp_tp
-
--- TODO link to `CategoryTheory.GroupidNMBase` in blueprint
-def groupoidNMBase1 : NaturalModelBase Ctx.{u+1} where
-  Ty := Ty
-  Tm := Tm
-  tp := tp
-  ext := ext
-  disp := disp
-  var := var
-  disp_pullback := isPullback_yonedaDisp_tp
-
-#exit
-def groupoidUHomSeq0 : NaturalModelBase.UHomSeq Ctx.{u} where
-  length := 0
-  objs i h := groupoidNMBase.{u}
-  homSucc' i h := False.elim (by cases i; simp at h; simp at h)
-
-def GroupoidUHomSeq1Objs (i : Nat) (h : i < 1) : NaturalModelBase Ctx.{u+1} :=
-  match i with
-  | 0 => groupoidNMBase.{u+1} -- bad
-  | 1 => sorry
-  | (n+2) => sorry
-
-def groupoidUHomSeq1 : NaturalModelBase.UHomSeq Ctx.{u+1} where
-  length := 1
-  objs i h := sorry
-  homSucc' i h := sorry
-
-end CategoryTheory
-#exit
-namespace CategoryTheory
-namespace GroupoidNM
+namespace GroupoidNaturalModel
 abbrev Ctx := AsSmall.{u} Grpd.{u,u}
 
-def Ctx.ofGroupoid (C : Type u) [Groupoid.{u} C] : Ctx.{u} :=
-  AsSmall.up.obj (Grpd.of C)
+namespace Ctx
+def ofGroupoid : Grpd.{u,u} ⥤ Ctx.{u} := AsSmall.up
 
-def Ctx.toGrpd (Γ : Ctx.{u}) : Grpd.{u,u} := AsSmall.down.obj Γ
+def toGrpd : Ctx.{u} ⥤ Grpd.{u,u} := AsSmall.down
+
+-- def Ctx.coreOfCategory (C : Type u) [Category.{v} C] : Ctx.{max w v u} :=
+--   Ctx.ofGroupoid (Core (AsSmall.{w} C))
+
+
+def core : Cat.{v,v+1} ⥤ Ctx.{max u (v + 1)} :=
+  Core.functor.{v,v+1}
+  ⋙ Grpd.asSmallFunctor.{u,v,v+1}
+  ⋙ Ctx.ofGroupoid.{max u (v + 1)}
+
+end Ctx
 
 @[simps] def catLift : Cat.{u,u} ⥤ Cat.{u,u+1} where
   obj x := Cat.of (ULift.{u + 1, u} x)
@@ -230,7 +103,7 @@ variable {Γ Δ : Ctx.{u}} {C D : Type (u+1)}
 /- The bijection y(Γ) → [-,C]   ≃   Γ ⥤ C -/
 def yonedaCatEquiv :
     (yoneda.obj Γ ⟶ yonedaCat.obj (Cat.of C))
-      ≃ Γ.toGrpd ⥤ C :=
+      ≃ Ctx.toGrpd.obj Γ ⥤ C :=
   Equiv.trans yonedaEquiv
     {toFun     := λ A ↦ ULift.upFunctor ⋙ A
      invFun    := λ A ↦ ULift.downFunctor ⋙ A
@@ -259,7 +132,7 @@ variable {Γ : Ctx.{u}} (A : yoneda.obj Γ ⟶ Ty)
 
 abbrev tp : Tm ⟶ Ty := yonedaCat.map (PGrpd.forgetToGrpd)
 
-abbrev ext : Ctx := Ctx.ofGroupoid (Groupoidal (yonedaCatEquiv A))
+abbrev ext : Ctx := Ctx.ofGroupoid.obj $ Grpd.of (Groupoidal (yonedaCatEquiv A))
 
 abbrev disp : ext A ⟶ Γ :=
   AsSmall.up.map (Grothendieck.forget (yonedaCatEquiv A ⋙ Grpd.forgetToCat))
@@ -267,10 +140,15 @@ abbrev disp : ext A ⟶ Γ :=
 abbrev var : (yoneda.obj (ext A) : Psh Ctx) ⟶ Tm :=
   yonedaCatEquiv.invFun (Groupoidal.toPGrpd (yonedaCatEquiv A))
 
+/-- `U.{v}` serves as the universe of `v`-small types.
+  This will be the "`Ty`" for the smaller natural models for universes. -/
+abbrev U : Psh Ctx.{max u (v + 1)} :=
+  yoneda.obj $ Ctx.core.obj.{v,u} $ Cat.of Grpd.{v,v}
+
 /-- The image of (roughly) `Groupoidal.toPGrpd : Grothendieck A ⥤ PGrpd`
   under `yonedaCat`.
   Used in the pullback diagram `isPullback_yonedaCatULiftGrothendieckForget_tp`
--/
+  -/
 abbrev yonedaCatMapToPGrpd :
     yonedaCat.obj (IsPullback.uLiftGrothendieck $ Groupoidal.toCat (yonedaCatEquiv A)) ⟶ Tm :=
   yonedaCat.map
@@ -288,7 +166,7 @@ abbrev yonedaCatMapGrothendieckForget :=
   Used in the pullback diagram `isPullback_yonedaCatULiftGrothendieckForget_tp`
 -/
 abbrev yonedaCatMapYonedaCatEquiv :
-    yonedaCat.obj (IsPullback.uLiftΓ.{u,u} Γ.toGrpd) ⟶ Ty :=
+    yonedaCat.obj (IsPullback.uLiftΓ.{u,u} $ Ctx.toGrpd.obj Γ) ⟶ Ty :=
   yonedaCat.map (Cat.homOf (ULift.downFunctor.{u,u} ⋙ (yonedaCatEquiv A)))
 
 /-- The image of the pullback `Grothendieck.Groupoidal.isPullback` under `yonedaCat` -/
@@ -325,7 +203,8 @@ theorem isPullback_yonedaDisp_yonedaCatULiftGrothendieckForget :
       (asSmallUp_comp_yoneda_iso_forgetToCat_comp_catLift_comp_yonedaCat.hom.app _)
       (yoneda.map (disp A))
       (yonedaCatMapGrothendieckForget A)
-      (asSmallUp_comp_yoneda_iso_forgetToCat_comp_catLift_comp_yonedaCat.hom.app Γ.toGrpd)
+      (asSmallUp_comp_yoneda_iso_forgetToCat_comp_catLift_comp_yonedaCat.hom.app
+        $ Ctx.toGrpd.obj Γ)
       :=
     IsPullback.of_horiz_isIso ⟨
       asSmallUp_comp_yoneda_iso_forgetToCat_comp_catLift_comp_yonedaCat.hom.naturality
@@ -339,12 +218,13 @@ theorem isPullback_yonedaDisp_tp :
   ext Δ F
   exact congr_fun (@A.naturality (Opposite.op Γ) Δ F.op) (CategoryStruct.id Γ)
 
-end GroupoidNM
-
-open GroupoidNM
-
--- TODO link to `CategoryTheory.GroupidNMBase` in blueprint
-def groupoidNMBase0 : NaturalModelBase Ctx.{u} where
+-- TODO link to this in blueprint
+/-- The natural model that acts as the ambient
+  model in which the other universes live.
+  Note that unlike the other universes this is *not* representable,
+  but enjoys having representable fibers that land in itself.
+-/
+def base : NaturalModelBase Ctx.{u} where
   Ty := Ty
   Tm := Tm
   tp := tp
@@ -353,31 +233,50 @@ def groupoidNMBase0 : NaturalModelBase Ctx.{u} where
   var := var
   disp_pullback := isPullback_yonedaDisp_tp
 
--- TODO link to `CategoryTheory.GroupidNMBase` in blueprint
-def groupoidNMBase1 : NaturalModelBase Ctx.{u+1} where
-  Ty := Ty
-  Tm := Tm
-  tp := tp
-  ext := ext
-  disp := disp
-  var := var
-  disp_pullback := isPullback_yonedaDisp_tp
+-- TODO link to this in blueprint
+/-- The natural model that acts as the classifier of `v`-large terms and types.
+  Note that unlike `GroupoidNaturalModel.base` this is representable,
+  but since representables are `max u (v + 1)`-large,
+  its representable fibers can be larger than itself.
+-/
+def baseU : NaturalModelBase Ctx.{max u (v + 1)} :=
+  @NaturalModelBase.pullback _ _ base.{max u (v + 1)}
+    (Ctx.core.obj.{v,u} $ Cat.of Grpd.{v,v}) 
+    (yonedaEquiv.symm $ Cat.homOf $
+      ULift.downFunctor
+      ⋙ AsSmall.down
+      ⋙ Core.inclusion Grpd.{v,v}
+      ⋙ Grpd.asSmallFunctor.{max u (v+1)})
 
-#exit
-def groupoidUHomSeq0 : NaturalModelBase.UHomSeq Ctx.{u} where
-  length := 0
-  objs i h := groupoidNMBase.{u}
-  homSucc' i h := False.elim (by cases i; simp at h; simp at h)
-
-def GroupoidUHomSeq1Objs (i : Nat) (h : i < 1) : NaturalModelBase Ctx.{u+1} :=
+def uHomSeqObjs (i : Nat) (h : i < 3) : NaturalModelBase Ctx.{2} :=
   match i with
-  | 0 => groupoidNMBase.{u+1} -- bad
-  | 1 => sorry
-  | (n+2) => sorry
+  | 0 => baseU.{0,2}
+  | 1 => baseU.{1,2}
+  | 2 => base
+  | (n+3) => by omega
 
-def groupoidUHomSeq1 : NaturalModelBase.UHomSeq Ctx.{u+1} where
-  length := 1
-  objs i h := sorry
-  homSucc' i h := sorry
+def uHomSeqHomSucc' (i : Nat) (h : i < 2) :
+    (uHomSeqObjs i (by omega)).UHom (uHomSeqObjs (i + 1) (by omega)) :=
+  match i with
+  | 0 => sorry
+  | 1 => sorry
+  | (n+2) => by omega
+
+/--
+  The groupoid natural model with two nested representable universes 
+-/
+def uHomSeq : NaturalModelBase.UHomSeq Ctx.{2} where
+  length := 2
+  objs := uHomSeqObjs
+  homSucc' := uHomSeqHomSucc'
+
+end GroupoidNaturalModel
+
+open GroupoidNaturalModel
+
+
 
 end CategoryTheory
+
+-- noncomputable
+end
