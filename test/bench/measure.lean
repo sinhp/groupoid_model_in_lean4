d601b976ab55d533e5b2e70759f7f1afd3bc0f02
rongshuge <shugerong@gmail.com>
2025-08-28 21:14:34 -0400

    remove some comments in measure

--- test/bench/measure.lean
+++ test/bench/measure.lean
@@ -3,82 +3,26 @@ import Mathlib.Tactic.MoveAdd
 import Std
 import Qq
 import Lean.Elab.Command
-import GroupoidModel.Syntax.Frontend.Translation
 import GroupoidModel.Syntax.Frontend.Commands
+
 -- 1. Load the .olean file with samples into the environment
 import bench.sample_llm
---import typechecker
+
 
 -- 2. Read all the `benchDef.n constants from the environment
 
 open Lean Elab Meta Term Leanternal Command
 open Qq
 
--- declare_theory mltt
-
--- /-! Tests to ensure the typechecker works. -/
-
--- /-! ## Universes -/
-
--- mltt def tp_univ : Type 1 := Type 0
-
--- /-! ## Functions -/
-
--- mltt def tp_pi_nondep : Type 1 := Type 0 → Type 0
-
--- mltt def tm_lam_nondep : Type 0 → Type 0 := fun x => x
-
--- mltt def tp_pi : Type 1 := (A : Type 0) → A → A
-
--- mltt def tm_lam : (A : Type 0) → A → A := fun _ a => a
-
--- mltt def tm_app : (A : Type 0) → A → (A → A) → A := fun _ a f => f a
-
--- /-! ## Products -/
-
--- mltt def tp_sigma : Type 1 :=
---   (A : Type) × A
-
--- mltt def tp_sigma_partial : (A : Type) → (B : A → Type) → Type :=
---   @Sigma
-
--- mltt def tm_pair_nondep : (_ : Type 1) × Type 1 :=
---   ⟨Type 0, Type 0⟩
-
--- -- Noncomputable due to Lean issue https://github.com/leanprover/lean4/issues/9692
--- mltt noncomputable def tm_pair : (A : Type 2) × A :=
---   ⟨Type 1, Type 0⟩
-
--- mltt def tm_fst : Type 2 :=
---   { fst := Type 1, snd := Type 0 : (A : Type 2) × A }.fst
-
--- mltt def tm_snd : Type 1 :=
---   { fst := Type 1, snd := Type 0 : (A : Type 2) × A }.snd
-
--- /-! ## Identity types -/
-
--- mltt def tp_id : Type 2 :=
---   @Identity (Type 1) Type Type
-
--- mltt def tm_refl : @Identity (Type 1) Type Type :=
---   @Identity.refl (Type 1) Type
-
--- mltt noncomputable def tm_idRec (A B : Type) (eq : @Identity Type A B) (a : A) : B :=
---   @Identity.rec Type A (fun T _ => T) a B eq
-
--- /-! ## Definitional equalities -/
-
--- mltt def defeq_el_code {A : Type} (a : A) : A :=
---   (fun (α : Type) (x : α) => x) ((fun (α : Type 1) (x : α) => x) Type A) a
-open String
+open Char
 def isSampleBenchDef (n : Name) : Bool :=
   match n with
   | .str (.str _ s2) s1 =>
     -- components come as: ... → "sample<n>" (s1) ← "benchDef_<m>" (s2) ← _ (rest)
     let ok1 := s1.startsWith "sample"
-      && Char.isDigit ((s1.drop 6).get 0)
+      && isDigit ((s1.drop 6).get 0)
     let ok2 := s2.startsWith "benchDef_"
-      && Char.isDigit ((s2.drop 9).get 0)
+      && isDigit ((s2.drop 9).get 0)
     ok1 && ok2
   | _ => false
 
