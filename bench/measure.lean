3fa94ec7ce6d0334122d16050febd367b8b19020
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2025-09-05 00:36:09 -0400

    feat: measure more sizes and impact of sharing

--- bench/measure.lean
+++ bench/measure.lean
@@ -1,5 +1,4 @@
 import Lean
-import Mathlib.Tactic.MoveAdd
 import GroupoidModel.Syntax.Frontend.Commands
 
 open Lean Elab Meta Term Leanternal Command
@@ -20,6 +19,9 @@ def timedSection {α : Type} {m : Type → Type} [Monad m] [MonadError m] [Monad
     | .heartbeats => withHeartbeats x
   catch ex =>
     throwError "{title} failed: {ex.toMessageData}"
+
+def shareQ {u : Level} {α : Q(Sort u)} (e : Q($α)) : Q($α) :=
+  ShareCommon.shareCommon' e
 
 /-- Measure effort taken by various components on definition `n`
 and append them to the NDJSON file `f`.
@@ -56,21 +58,22 @@ elab "#measure" n:ident f:str b:num : command => liftTermElabM do
   let axioms : Q(Axioms Name) := q(Axioms.empty Name)
   have wf_axioms : Q(($axioms).Wf) := q(Axioms.empty_wf Name)
 
-  let (value, dt_typecheck) ← timedSection "typechecking" meas do
+  let ((value, vT, wf_nfTp, wf_val), dt_typecheck) ← timedSection "typechecking" meas do
     let Twf ← checkTp q($axioms) q($wf_axioms) q([]) q($l) q($T)
     let ⟨vT, vTeq⟩ ← evalTpId q(show TpEnv Lean.Name from []) q($T)
     let twf ← checkTm q($axioms) q($wf_axioms) q([]) q($l) q($vT) q($t)
-    -- let t₁ ← IO.monoNanosNow
+    have wf_nfTp := q($vTeq .nil <| $Twf .nil)
+    have wf_val := q($twf .nil $wf_nfTp)
     let value : Q(CheckedDef $axioms) := q(
       { l := $l
         tp := $T
         nfTp := $vT
-        wf_nfTp := $vTeq .nil <| $Twf .nil
+        wf_nfTp := $wf_nfTp
         val := $t
-        wf_val := $twf .nil <| $vTeq .nil <| $Twf .nil
+        wf_val := $wf_val
       }
     )
-    pure value
+    pure (value, vT, wf_nfTp, wf_val)
 
   let (_, dt_rkernel) ← timedSection "rkernel" meas do
     -- Important option: ensures that addDecl will wait for the kernel.
@@ -84,13 +87,38 @@ elab "#measure" n:ident f:str b:num : command => liftTermElabM do
       safety := .safe
     }
 
+  let (_, dt_rkernel_shared) ← timedSection "rkernel (shared)" meas do
+    -- Important option: ensures that addDecl will wait for the kernel.
+    withOptions (fun opts => Elab.async.set opts false) do
+    withoutModifyingEnv <| addDecl <| .defnDecl {
+      name := d.name ++ `benchl
+      levelParams := []
+      type := q(CheckedDef $axioms)
+      value := shareQ q($value)
+      hints := .regular 0 -- TODO: what height?
+      safety := .safe
+    }
+
+  -- Note: size with full sharing is `(ShareCommon.shareCommon' e).numObjs`
   IO.FS.withFile f.getString .append fun fTimes => do
     let j : Json := json%
       { name : $(d.name),
-        sz: $(d.value.size),
+        sz_tp: $(← d.type.numObjs),
+        sz_val: $(← d.value.numObjs),
+        sz_deep_tp: $(← T.numObjs),
+        sz_deep_tp_shared: $(← (ShareCommon.shareCommon' T).numObjs),
+        sz_deep_nfTp: $(← vT.numObjs),
+        sz_deep_wf_nfTp: $(← wf_nfTp.numObjs),
+        sz_deep_val: $(← t.numObjs),
+        sz_deep_val_shared: $(← (ShareCommon.shareCommon' t).numObjs),
+        sz_deep_wf_val: $(← wf_val.numObjs),
+        sz_deep_wf_val_shared: $(← (ShareCommon.shareCommon' wf_val).numObjs),
+        sz_nfTp_deep: $(← T.numObjs),
         t_kernel: $dt_kernel,
         t_translate: $dt_translate,
         t_typecheck: $dt_typecheck,
-        t_rkernel: $dt_rkernel }
+        t_rkernel: $dt_rkernel,
+        t_rkernel_shared: $dt_rkernel_shared
+      }
     fTimes.putStrLn j.compress
     fTimes.flush
