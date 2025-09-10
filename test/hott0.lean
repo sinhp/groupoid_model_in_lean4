7c6e51e098995edc63c1407c98382e5c3dda4820
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2025-09-10 10:47:04 -0400

    fix: statement of HoTT0

--- test/hott0.lean
+++ test/hott0.lean
@@ -6,29 +6,35 @@ declare_theory hott0
 
 namespace HoTT0
 
-hott0 axiom funext₀₀ {A B : Type} (f g : A → B)
-  (ext : ∀ (x : A), Identity (f x) (g x)) : Identity f g
-
-hott0 def isInv₀₀ {A B : Type} (f : A → B) (g : B → A) : Type :=
+hott0 def isSection₀₀ {A B : Type} (f : A → B) (g : B → A) : Type :=
   ∀ (a : A), Identity (g (f a)) a
 
 hott0 def isEquiv₀₀ {A B : Type} (f : A → B) : Type :=
   Σ (g : B → A),
     Σ (h : B → A),
-      Σ (_ : isInv₀₀ f g),
-        isInv₀₀ h f
+      Σ (_ : isSection₀₀ f g),
+        isSection₀₀ h f
 
-hott0 def isInv₁₀ {A : Type 1} {B : Type} (f : A → B) (g : B → A) : Type 1 :=
+hott0 def happly {A : Type} {B : A → Type} {f g : (a : A) → B a} :
+    Identity f g → (a : A) → Identity (f a) (g a) :=
+  fun h _ => h.rec .rfl₀
+
+hott0
+  /-- HoTT book, Axiom 2.9.3. -/
+  axiom funext₀₀ {A : Type} {B : A → Type} (f g : (a : A) → B a) :
+    isEquiv₀₀ (@happly _ _ f g)
+
+hott0 def isSection₁₀ {A : Type 1} {B : Type} (f : A → B) (g : B → A) : Type 1 :=
   ∀ (a : A), Identity (g (f a)) a
 
-hott0 def isInv₀₁ {A : Type} {B : Type 1} (f : A → B) (g : B → A) : Type :=
+hott0 def isSection₀₁ {A : Type} {B : Type 1} (f : A → B) (g : B → A) : Type :=
   ∀ (a : A), Identity (g (f a)) a
 
 hott0 def isEquiv₁₀ {A : Type 1} {B : Type} (f : A → B) : Type 1 :=
   Σ (g : B → A),
     Σ (h : B → A),
-      Σ (_ : isInv₁₀ f g),
-        isInv₀₁ h f
+      Σ (_ : isSection₁₀ f g),
+        isSection₀₁ h f
 
 hott0 def transport₀ {A B : Type} (h : Identity A B) (a : A) : B :=
   h.rec a
@@ -46,6 +52,6 @@ hott0 def isSet₀ (A : Type) : Type :=
   ∀ (a b : A), isProp₀ (Identity a b)
 
 hott0
-  /-- The univalence axiom for sets. -/
-  axiom setUv₀₀ {A B : Type} (ha : isSet₀ A) (hb : isSet₀ B) :
+  /-- The univalence axiom for sets. See HoTT book, Axiom 2.10.3. -/
+  axiom setUv₀₀ {A B : Type} (A_set : isSet₀ A) (B_set : isSet₀ B) :
     isEquiv₁₀ (@Identity.toEquiv₀₀ A B)
