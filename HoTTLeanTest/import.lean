719bcd6bcc4f1d7d293c0cb16f67db0deef5b37d
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2026-02-11 17:47:10 -0500

    chore: trace and doc

--- HoTTLeanTest/import.lean
+++ HoTTLeanTest/import.lean
@@ -1,6 +1,9 @@
 import HoTTLeanTest.basic
 
-/-! Test importing a theory. -/
+/-! Test importing a theory.
+
+Note: this is no longer as test-worthy since we deprecated theory-environments,
+and `@[reflect]` just uses the ordinary Lean environment. -/
 
 @[reflect]
 noncomputable def tm_refl'' : tp_id := tm_refl'
